dbf18687f8a62531aac9127c92f4b3cdd6f15260
plms <askdell@sina.com>
2022-07-06 22:48:40 +0800

    RestTemplate之getForEntity(..)练习

--- spring-cloud-project-cluster/ben-consumer-80/src/main/java/com/springcloud/learn/controller/ConsumerController.java
+++ spring-cloud-project-cluster/ben-consumer-80/src/main/java/com/springcloud/learn/controller/ConsumerController.java
@@ -2,18 +2,27 @@ package com.springcloud.learn.controller;
 
 import com.springcloud.learn.entity.CommonResult;
 import com.springcloud.learn.entity.PaymentUser;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 @Controller
 @RequestMapping(value="/consumer")
 public class ConsumerController {
 
     //public static final String PAYMENT_URL = "http://localhost:8081";    //单服务模块访问地址写法
-    public static final String PAYMENT_URL = "http://ANN-PAYMENT";   //访问集群时的写法，这个路径是集群中多个模块的设置好的共同的名字
+    public static final String PAYMENT_URL = "http://ANN-PAYMENT";   //访问集群时的写法，这个路径是集群中多个模块的设置好的共同的名字，也是注册在Eureka注册中心的名字
 
     @Resource   //这里使用@Resource注解也可以
     private RestTemplate restTemplate;
@@ -22,9 +31,36 @@ public class ConsumerController {
     @GetMapping("/get/{id}")
     @ResponseBody
     public CommonResult<PaymentUser> getUserById(@PathVariable(value="id") String id){
-        //一，从这里通过http工具调用ann-payment模块的服务，实现了跨模块调用获取数据
+        //一，从这里通过http工具调用ann-payment模块的服务，实现了跨模块调用获取数据；
+        //   使用getForObject(..)方法；
         CommonResult<PaymentUser> commonResult = restTemplate.getForObject(PAYMENT_URL + "/payment/get/" + id, CommonResult.class);  //注意地址拼写
         return commonResult;
+    }
+
+    //二，RestTemplate.java的getForEntity(..)方法练习，同时复习Servlet;
+    @GetMapping("/getForEntity/{id}")
+    public void getForEntity(@PathVariable(value="id") String id, HttpServletRequest request, HttpServletResponse response){
+        ResponseEntity<String> responseEntity = restTemplate.getForEntity(PAYMENT_URL + "/payment/get/" + id, String.class);
+        HttpHeaders headers = responseEntity.getHeaders();
+        MediaType contentType = headers.getContentType();
+        String body = responseEntity.getBody();
+        HttpStatus statusCode = responseEntity.getStatusCode();
+        int statusCodeValue = responseEntity.getStatusCodeValue();
+        response.setContentType("text/html;charset=utf-8");
+        PrintWriter writer = null;
+        try {
+             writer = response.getWriter();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        writer.print("headers" + headers.toString() + "<br>");
+        writer.print("contentType:" + contentType + "<br>");
+        writer.print("statusCode:" + statusCode + "<br>");
+        writer.print("statusCodeValue:" + statusCodeValue + "<br>");
+        //输出流清空，关闭
+        writer.flush();
+        writer.close();
     }
 
     //进行添加数据
