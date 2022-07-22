c1ca0d12204a74941434a61bda2fd8bde036c173
plms <askdell@sina.com>
2022-07-22 17:14:29 +0800

    追加dashboard配置笔记

--- spring-cloud-project-hystrix/jenny-consumer-hystrix-dashboard-90/src/main/java/com/springcloud/learn/DashboardApplication90.java
+++ spring-cloud-project-hystrix/jenny-consumer-hystrix-dashboard-90/src/main/java/com/springcloud/learn/DashboardApplication90.java
@@ -8,6 +8,7 @@ import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboar
  * 1,Hystrix的dashboard只有一个启动类，没有业务处理层；
  * 2,访问路径：http://localhost:90/hystrix ，注意加:hystrix；
  * 3,在dashboard页面对应位置输入要检测的服务模块名称即可；
+ * 4,(重点)在被监视的模块的主启动类里进行ServletRegistrationBean的相关配置，见ian-consumer-hystrix-83模块主启动类：ConsumerHystrixApplication83.java
  * */
 
 @SpringBootApplication
